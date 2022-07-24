057c89c81b7924d42cf1c121b8d6b5611c6af08c
Pavan Kumar Reddy Yannam <yannam@localhost.localdomain>
2022-07-24 22:41:08 +0300

    conducted basic experiments

--- pipeline/time_effect_visualize.sh
+++ pipeline/time_effect_visualize.sh
@@ -1,11 +1,10 @@
 #! /bin/bash
 
-# python3 src/main.py -v --window 1 48 --table inputevents --visualize "Packed Red Blood Cells"	Hematocrit
-# python3 src/main.py -v --window 1 48 --table inputevents --visualize "Packed Red Blood Cells" Hemoglobin
-# python3 src/main.py -v --window 1 48 --table inputevents --visualize "Packed Red Blood Cells" "Red Blood Cells"
-# python3 src/main.py -v --window 1 48 --table inputevents --visualize "OR Crystalloid Intake" pO2
-# python3 src/main.py -v --window 1 48 --table inputevents --visualize "OR Cell Saver Intake" pO2
+python3 src/main.py -v --window 1 48 --table inputevents --visualize "Packed Red Blood Cells"	Hematocrit
+python3 src/main.py -v --window 1 48 --table inputevents --visualize "Packed Red Blood Cells" Hemoglobin
+python3 src/main.py -v --window 1 48 --table inputevents --visualize "Packed Red Blood Cells" "Red Blood Cells"
+python3 src/main.py -v --window 1 48 --table inputevents --visualize "OR Crystalloid Intake" pO2
+python3 src/main.py -v --window 1 48 --table inputevents --visualize "OR Cell Saver Intake" pO2
+python3 src/main.py -v --window 1 48 --table inputevents --visualize "Insulin - Regular" Glucose
 
-python3 src/main.py -v -p --window 1 48 --table prescriptions --visualize Insulin Glucose --meds 9400 --sub-pairs 1000
-
-# python3 src/main.py -v --window 1 48 --table inputevents --visualize "Insulin - Regular" Glucose
+python3 src/main.py -v --window 1 48 --table prescriptions --visualize Insulin Glucose
