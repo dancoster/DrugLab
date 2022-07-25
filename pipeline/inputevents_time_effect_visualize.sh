9557e0be4d9197cdc356d59d436ad2c64a2df6c4
Pavan Kumar Reddy Yannam <yannam@localhost.localdomain>
2022-07-25 16:21:19 +0300

    added an0other pair for visualization analysis

--- pipeline/inputevents_time_effect_visualize.sh
+++ pipeline/inputevents_time_effect_visualize.sh
@@ -6,3 +6,4 @@ python3 src/main.py -v --window 1 48 --table inputevents --visualize "Packed Red
 python3 src/main.py -v --window 1 48 --table inputevents --visualize "OR Crystalloid Intake" pO2
 python3 src/main.py -v --window 1 48 --table inputevents --visualize "OR Cell Saver Intake" pO2
 python3 src/main.py -v --window 1 48 --table inputevents --visualize "Insulin - Regular" Glucose
+python3 src/main.py -v --window 1 48 --table inputevents --visualize LR Hemoglobin
