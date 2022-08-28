5fbe6648fad671c565eb029c9b40890d9c4a4663
Pavan Kumar Reddy Yannam <yannam@localhost.localdomain>
2022-08-28 15:55:22 +0300

    test cases for new correlation analysis code

--- pipeline/test.sh
+++ pipeline/test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python3 src/main.py -p --window 1 48 --meds 200 --sub-pairs 200
+python3 src/main.py -v  --visualize "Insulin - Regular" Glucose --table inputevents --window 1 24 --meds 200 --sub-pairs 50 --stratify -a 50 60 -e WHITE -b 18.5 24.9 -g M
