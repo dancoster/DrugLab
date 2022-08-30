c9279f44d31a429fb0654b4682ee8a2c7fbb4fb2
Pavan Kumar Reddy Yannam <yannam@localhost.localdomain>
2022-08-30 13:23:39 +0300

    error8

--- src/analysis/time_effect_analysis.py
+++ src/analysis/time_effect_analysis.py
@@ -85,15 +85,14 @@ class TimeEffect(Analysis):
 
         # 'Insulin - Regular'
         # 'Glucose'
-        self.logger.info(self.patient_presc, self.lab_measurements)
         drug_lab, before1, after1 = Analysis.labpairing(presc, self.patient_presc, self.lab_measurements, lab, type=self.table, med1=self.data.med1, med2=self.data.med2, window=window)
 
-        self.logger.info('Before Subjects: ', drug_lab, after1, before1)
+        self.logger.info(f'Before Subjects: , {drug_lab}, {after1}, {before1}')
 
         subjects = list(drug_lab['SUBJECT_ID'].unique())
 
-        self.logger.info('Data: ', after1, before1)
-        self.logger.info('Params : ', after_window, before_window)
+        self.logger.info(f'Data: , {after1}, {before1}')
+        self.logger.info(f'Params : , {after_window}, {before_window}')
         
         if method=='before-after':            
             if after_window is not None:
@@ -104,7 +103,6 @@ class TimeEffect(Analysis):
                             after1['timeFromPrescription']<datetime.timedelta(hours=after_window[1])
                         )
                     )]                   
-                self.logger.info(after1)
             if self.table=='inputevents':
                 before1['timeFromPrescription'] = before1['timeFromPrescription'].apply(lambda x : round(x.total_seconds()/3600, 2) )
                 before1 = before1.sort_values(by='timeFromPrescription')
@@ -121,12 +119,12 @@ class TimeEffect(Analysis):
             after1 = after1[after1['SUBJECT_ID'].isin(before1['SUBJECT_ID'])]
             before1 = before1[before1['SUBJECT_ID'].isin(after1['SUBJECT_ID'])]
 
-        self.logger.info(after1, before1)
+        self.logger.info(f'{after1}, {before1}')
 
         reg_anal_res, _, _ = Analysis.interpolation(subjects, before1)
         if method=='estimate':
             e = pd.DataFrame(reg_anal_res)
-            self.logger.info(e)
+            self.logger.info(f'{e}')
             e = e.rename(columns={'subjectID':'SUBJECT_ID'})
             estimate = e['estimated']
         if method=='before-after':
