eb6ed0dcef817fc260c31432211daf86eed0af9a
Pavan Reddy <ypavan2802@gmail.com>
2023-04-18 20:17:15 +0530

    feat(Notebooks/Scripts): Notebooks and scripts used for experimentation and results generation

--- hirid-server-run-code/run_hirid_extract.py
+++ hirid-server-run-code/run_hirid_extract.py
@@ -27,8 +27,8 @@ def setup_io_config(root_path, data_path=None):
     mimic_path = os.path.join(f"{root_path}", "results")
 
     # HIRID
-    hirid_data = f'{root_path}/data/hirid-a-high-time-resolution-icu-dataset-1.1.1/raw_stage/'
-    hirid_path = f'{root_path}/data/hirid-a-high-time-resolution-icu-dataset-1.1.1'
+    hirid_data = f'{data_path}/hirid-a-high-time-resolution-icu-dataset-1.1.1/raw_stage/'
+    hirid_path = f'{root_path}/results/hirid/'
     
     return mimic_data, mimic_path, hirid_data, hirid_path
 
@@ -48,38 +48,17 @@ def setup_stratification_config():
 # root_path ="C:\\Users\\danco\\My Drive\\Master\\Datasets\\MIMIC iii"
 # root_path = "/Users/pavan/Library/CloudStorage/GoogleDrive-f20190038@hyderabad.bits-pilani.ac.in/My Drive/TAU/Code/DrugLab"
 root_path = "/home/gaga/yannam/DrugLab"
-data_path = "/home/gaga/data/physionet"
+data_path = "/home/gaga/data/physionet/HiRiD"
 data, res, raw_path, res_path = setup_io_config(root_path=root_path, data_path=data_path)
 
 # Stratification Config
 gender, age_a, age_b, ethnicity, lab_mapping, before_windows, after_windows = setup_stratification_config()
-# MIMIC
-mimic_parser = mimic.MIMICParser(data=data, res=res, gender=gender, age_b=age_b, age_a=age_a, ethnicity=ethnicity, load="AUTOMATIC_MAPPING_MIMIC")
-m_med1, m_med2, m_labs = mimic_parser.parse(use_pairs=False, load_from_raw=True, load_raw_chartevents=True)
 
-## Querier
-mimic_data_querier = querier.DatasetQuerier(
-    data = data,
-    res = res,
-    t_labs=m_labs, 
-    t_med1=m_med1, 
-    t_med2=m_med2,
-    gender=gender, 
-    age_b=age_b, 
-    age_a=age_a, 
-    ethnicity=ethnicity, 
-    lab_mapping=lab_mapping
-)
-# query pairs for all medication and lab tests
-m_final_lab_med_data = mimic_data_querier.generate_med_lab_data(before_windows, after_windows)
+# HIRID
+hirid_mapping = constants.HIRID_MAPPING
+lab_parts = (0,100)
+hirid_parser = hirid.HiRiDParser(data=raw_path, res=res_path, gender=gender, age_b=age_b, age_a=age_a, load="MANUAL_MAPPING_HIRID")
+h_med1, h_med2, h_labs = hirid_parser.parse(lab_parts=lab_parts)
 
-## Discovery Analysis for the queried medication and lab test pairs in the chosen before and after windows
-analyzer = discovery.ClinicalDiscoveryAnalysis(m_final_lab_med_data)
-types_l = ["abs", "mean", "std", "trends"]
-pvals_med_lab = analyzer.analyze(before_windows, after_windows, min_patients=100, types_l=types_l)
-pval, hard, bonferroni, fdr = analyzer.generate_significant(pvals_med_lab.dropna(subset=["TTest Paired"]))
-
-fdr.to_csv(f"temp_mimic_sig_med_lab_pairs_fdr_{1}.csv")
-bonferroni.to_csv(f"temp_mimic_sig_med_lab_pairs_bonferroni_{1}.csv")
-hard.to_csv(f"temp_mimic_sig_med_lab_pairs_hard_{1}.csv")
-pval.to_csv(f"temp_mimic_sig_med_lab_pairs_pval_{1}.csv")
+lab_with_index = h_labs[["HADM_ID", "AGE", "GENDER", "LabTimeFromAdmit", "VALUENUM", "ITEMID"]].pivot_table("VALUENUM", ["HADM_ID", "AGE", "GENDER",  "LabTimeFromAdmit"], "ITEMID")
+lab_with_index.to_csv(os.path.join(res_path, f"hirid_extract_{lab_parts}.csv"))
